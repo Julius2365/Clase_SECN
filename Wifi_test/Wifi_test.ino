370e97ad31fd8245d5d3449e5e606c1c738a5b96
Julio Hernandez <julio.hernandez.236541@ciencias.unam.mx>
2022-10-02 01:04:56 -0500

    Wifi & SPIFFS

---
+++ Wifi_test/Wifi_test.ino
@@ -0,0 +1,41 @@
+/*
+ *  This sketch sends data via HTTP GET requests to data.sparkfun.com service.
+ *
+ *  You need to get streamId and privateKey at data.sparkfun.com and paste them
+ *  below. Or just customize this script to talk to other HTTP servers.
+ *
+ */
+
+#include <WiFi.h>
+
+const char* ssid     = "Totalplay-C4A5";
+const char* password = "C4A58278SmQTCHy3";
+
+void setup()
+{
+    Serial.begin(115200);
+    delay(10);
+
+    // We start by connecting to a WiFi network
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void loop() {
+
+}
