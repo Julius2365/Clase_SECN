9875f6a5c94d195773778fd40750e8082b3974bf
Julio Hernandez <julio.hernandez.236541@ciencias.unam.mx>
2022-10-09 17:26:35 -0500

    JsonParser & Interrupciones

--- json/json.ino
+++ json/json.ino
@@ -1,71 +1,49 @@
-// Leer Json desde Serial, SD o SPIFFS 
-// Serializar un Json e imprimirlo por serial
+// Leer Json desde SPIFFS 
+// Deserializar un Json e imprimirlo por serial
 
-#include <Adafruit_BMP085.h>
 #include <ArduinoJson.h>
 #include <SPIFFS.h>
-//#include <Credentials.h>
 
 //Payload creado con snprintf
 #define BUFFER_LEN  256
 char payload[BUFFER_LEN]; //Payload Serializado
-String json_string;
+//String json_string;
 
 //Payload crearon con Json parser
 //Doc: https://arduinojson.org/
 //Ticket: Usar reservación de memoria en stack
 //Usar el json assistant para calcular el LEN necesario para cada documento
+
 #define JSON_BUFFER_INCOMING_LEN 200
 #define JSON_BUFFER_OUTGOING_LEN 200
 DynamicJsonDocument payload_in(JSON_BUFFER_INCOMING_LEN); //Json desde SPIFFS,SD,Serial o MQTT (Stream)
 DynamicJsonDocument payload_out(JSON_BUFFER_OUTGOING_LEN);//Json hacia SPIFFS,SD,Serial o MQTT (Stream)
 
-//Configuración de BMP y LED
-#define PIN_LED 32
-Adafruit_BMP085 bmp;
-
 void setup() {
   
   //Inicializar perifericos
-  Serial.begin(115200);
-  bmp.begin(0x77); 
-  pinMode(PIN_LED,OUTPUT);
+  Serial.begin(115200); 
   SPIFFS.begin(true);
-  File file_json;
-
-  //Leer el archivo con Json desde SPIFFS.
-  file_json = SPIFFS.open("/json_test.txt", FILE_READ);
-
-  //Guardar Json en variable
-  while (file_json.available()) {
-    json_string = file_json.readString();
-  }   
-  file_json.close(); 
-
-  //Serial.println(json_string); 
-
-  //Json parser deserializer
-  //Parseo desde el archivo 
-  file_json = SPIFFS.open("/json_test.txt", FILE_READ);
-  deserializeJson(payload_in,file_json);
-  file_json.close(); 
-
-  //Extracción de atributos
-  const char* message = payload_in["message"];
-  const char* origen = payload_in["origen"];
 
   
-  Serial.println(message);Serial.println(origen);
 
-
+  
 }
 
 
 void loop() {
+    
+  File file_json;
+  file_json = SPIFFS.open("/json_test.txt", FILE_READ);
+  deserializeJson(payload_in,file_json);
+  
+  file_json.close(); 
 
+  //Extracción de atributos
+  const char* nombre_alumno = payload_in["nombre_alumno"];
 
-   
-   Serial.println(json_string);
-   delay(5000);
+  Serial.print("nombre_alumno: ");
+  Serial.println(nombre_alumno);
+  delay(5000);
                       
 }
